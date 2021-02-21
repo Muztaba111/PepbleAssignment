546907edc0efcd626850c0c53bd81180cccaf004
Iltea alam <Iltea alam@Ilteaalam-PC>
2021-02-21 22:54:10 +0530

    commit

--- PepbleTest/src/test/java/testrunners/MyTestRunner.java
+++ PepbleTest/src/test/java/testrunners/MyTestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "C:\\Users\\Muztaba alam\\eclipse-workspace\\PepbleTest\\src\\test\\resources\\AppFeatures"
+		features = "C:\\Users\\\\Iltea alam\\eclipse-workspace\\PepbleTest\\src\\test\\resources\\AppFeatures"
 		,glue = {"stepDefinations","AppHooks"},
 		plugin = {"pretty","html:target/MyReports/reports.html","junit:target/MyReports/reports.xml",
 				"json:target/MyReports/reports.json"},
